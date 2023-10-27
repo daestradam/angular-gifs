81344a7d36aca0b5ebf4f2f215aa93f1f75e017f
Daniel <daestradam@unal.edu.co>
2023-10-27 01:09:21 -0500

    card-list with gifs and sidebar linked

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -42,7 +42,6 @@ export class GifsService {
     this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params})
       .subscribe(resp => {
         this.gifList=resp.data;
-        console.log({gifs: this.gifList});
       });
   }
 
